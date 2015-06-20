769d8269c0eff7e7be6789d5169680e6a34236bc
srhoades28 <srhoades28@gmail.com>
2015-06-20 11:10:01 -0600

    Still trying to figure out how to autoload the vendor files and then use the soundcloud facade file in my controllers.

--- application/controllers/Soundcloud.php
+++ application/controllers/Soundcloud.php
@@ -1,11 +1,16 @@
 <?php
-class Soundcloud extends CI_Controller{
-	
-	use Njasm\Soundcloud\SoundcloudFacade;
 
+class Soundcloud extends CI_Controller{
 	
 	public function __contruct(){
 		parent::__construct();
+
+		
+	}
+	
+	public function index(){
+	$this->load->view('main_header');
+		
 	}
 
 }
